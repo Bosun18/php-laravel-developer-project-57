b5eefd248bc6257f1a335b6b00f0c3b922534304
Bosun18 <beliy-81@mail.ru>
2024-01-16 16:07:42 +0300

    update LabelFactory

--- database/factories/LabelFactory.php
+++ database/factories/LabelFactory.php
@@ -3,6 +3,7 @@
 namespace database\factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Random\RandomException;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Label>
@@ -13,11 +14,13 @@ class LabelFactory extends Factory
      * Define the model's default state.
      *
      * @return array<string, mixed>
+     * @throws RandomException
      */
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->unique()->word(),
+            'description' => fake()->sentence(random_int(6, 15)),
         ];
     }
 }
